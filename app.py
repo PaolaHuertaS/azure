079c32a4bc8541a010b01fd5b1e4183c70aacc79
PaoHuerta <139302970+PaolaHuertaS@users.noreply.github.com>
2025-06-16 00:28:36 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 from flask import Flask, render_template
-import os
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    proveedores = obtener_proveedores()
-    return render_template("index.html")
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8000)
+
